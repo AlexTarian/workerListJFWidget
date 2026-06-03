dce746814b0e2f279b7c5e55635379ab00da99a8
AlexTarian <94810153+AlexTarian@users.noreply.github.com>
2026-06-03 16:11:50 -0400

    Add countries.js with country data

---
+++ countries.js
@@ -0,0 +1,7 @@
+const COUNTRIES = [
+  { name: "Mexico", iso: "MX", phoneCode: "+52" },
+  { name: "South Africa", iso: "ZA", phoneCode: "+27" },
+  { name: "Jamaica", iso: "JM", phoneCode: "+1" },
+  { name: "Guatemala", iso: "GT", phoneCode: "+502" },
+  { name: "Honduras", iso: "HN", phoneCode: "+504" }
+];
